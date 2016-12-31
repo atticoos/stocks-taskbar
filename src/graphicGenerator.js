2fc02f905a4912289a4cacb9d50c8c17a4b46dc5
Atticus White <contact@atticuswhite.com>
2016-12-31 17:48:09 -0500

    canvas integration for dynamic ticker images

---
+++ src/graphicGenerator.js
@@ -0,0 +1,50 @@
+import Canvas from 'canvas';
+import {nativeImage} from 'electron';
+
+const font = '12px Helvetica';
+
+function measureText (text) {
+  var canvas = new Canvas();
+  var ctx = canvas.getContext('2d');
+  ctx.font = font;
+  return ctx.measureText(text);
+}
+
+export function createTextImage (text) {
+  var canvas = new Canvas(measureText(text).width, 20);
+  var ctx = canvas.getContext('2d');
+  ctx.font = font;
+  ctx.fillText(text, 0, 15);
+  return canvas;
+  return nativeImage.createFromBuffer(canvas.toBuffer(), {
+    width: ctx.measureText(text).width,
+    height: 20,
+    scaleFactor: 1.0
+  });
+}
+
+export function createTickerImage (text, offset = 0) {
+  console.log({text})
+  var canvas = new Canvas(200, 20);
+  var ctx = canvas.getContext('2d');
+  ctx.font = font;
+  var {width} = ctx.measureText(text);
+
+  var img = new Canvas.Image();
+  img.src = createTextImage(text).toBuffer();
+  console.log('width', width)
+  console.log('at offset', -offset)
+  ctx.drawImage(img, -offset, 0, width, 20);
+
+  return nativeImage.createFromBuffer(canvas.toBuffer(), {
+    width: 200,
+    height: 20,
+    scaleFactor: 1.0
+  });
+
+  return nativeImage.createFromBuffer(createTextImage(text).toBuffer(), {
+    width: 200,
+    height: 20,
+    scaleFactor: 1.0
+  });
+}
