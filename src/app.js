3be782afe5498c8675b87300f1903189abcee449
Atticus White <contact@atticuswhite.com>
2017-01-02 13:31:17 -0500

    ui integration

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import menubar from 'menubar';
+import path from 'path';
 import {buildTray} from './tray';
 
-var mb = menubar();
+var mb = menubar({
+  index: 'file://' + path.normalize(path.join(__dirname, 'index.html'))
+});
 
 mb.on('ready', () => {
   buildTray(mb.tray);
