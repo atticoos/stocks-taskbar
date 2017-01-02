bcfadd2f1127d12fce766877c2d6cfa73d24367f
Atticus White <contact@atticuswhite.com>
2017-01-02 13:18:01 -0500

    slight cleanup

--- src/graphicGenerator.js
+++ src/graphicGenerator.js
@@ -13,23 +13,18 @@ function measureText (text) {
 }
 
 function createCanvasFromData (quotes) {
-  console.log('wtf', quotes)
   var measurements = quotes.map(quote => {
     var percent = Math.abs(quote.ChangePercent).toFixed(2) + '%';
     percent = (quote.ChangePercent > 0 ? '+' : '-') + percent;
-    var symbolSize = measureText(quote.Symbol).width;
-    var percentSize = measureText(percent).width;
-    //var percentSize = measureText(Math.abs(quote.ChangePercent).toFixed(2) + '%').width;
 
     return {
       symbol: {
         value: quote.Symbol,
-        size: symbolSize
+        size: measureText(quote.Symbol).width
       },
       percent: {
-        //value: Math.abs(quote.ChangePercent),
         value: percent,
-        size: percentSize
+        size: measureText(percent).width
       },
       up: quote.ChangePercent >= 0
     }
@@ -39,8 +34,6 @@ function createCanvasFromData (quotes) {
     var itemTotal = item.symbol.size + SPACE + TRI + SPACE + item.percent.size + SPACE + SPACE;
     return total + itemTotal;
   }, 0);
-
-  console.log({totalSize})
 
   var canvas = new Canvas(totalSize, 20);
   var ctx = canvas.getContext('2d');
@@ -99,17 +92,12 @@ export function createTextCanvas (text) {
 }
 
 export function createTickerImage (data, offset = 0) {
-  //console.log({text})
   var canvas = new Canvas(200, 20);
   var ctx = canvas.getContext('2d');
   ctx.font = font;
-  //var {width} = ctx.measureText(text);
 
   var img = new Canvas.Image();
-  //img.src = createTextCanvas(text).toBuffer();
   img.src = createCanvasFromData(data).toBuffer();
-  console.log('width', img.width)
-  console.log('at offset', -offset)
   ctx.drawImage(img, -offset, 0, img.width, 20);
 
   return nativeImage.createFromBuffer(canvas.toBuffer(), {
