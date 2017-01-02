3479849a7246489166cadabcbd0c91b02d6d8992
Atticus White <contact@atticuswhite.com>
2017-01-02 16:23:09 -0500

    cleanup

--- src/tray.js
+++ src/tray.js
@@ -1,9 +1,13 @@
 import {Menu, Tray} from 'electron';
-import path from 'path';
-import stockSource from './stocks';
 import {createTickerImage} from './graphicGenerator';
 import {ipcMain} from 'electron';
+import path from 'path';
+import stockSource from './stocks';
 import * as ActionTypes from './window/actions/types';
+
+const MAX_FRAMES = 200;
+const FRAME_CHUNK_SIZE = 20;
+const FPS = 40;
 
 var mockData = [
   {Symbol: 'AAPL', ChangePercent: 3.02},
@@ -13,15 +17,12 @@ var mockData = [
 ];
 
 export function buildTray (tray) {
-  console.log('building tray')
   var animateFrames = createTrayFrameAnimator(tray, FPS);
 
   var icon = path.normalize(path.join(__dirname, 'icon.png'));
   tray.setToolTip('Stock Ticker');
 
   generateFrames(mockData).then(frames => animateFrames(frames));
-
-  //tray.setContextMenu(buildStockMenu(mockData));
 
   /*
   stockSource.subscribe(
@@ -30,9 +31,8 @@ export function buildTray (tray) {
       tray.setContextMenu(menuItems)
     }
     );
-    */
+  */
 
-  //var stopRotation = rotateTrayIcon(tray, mockData);
   ipcMain.on('renderer-action', (event, action) => {
     console.log('action', action.type)
     switch (action.type) {
@@ -62,16 +62,6 @@ export function buildTray (tray) {
   return tray;
 }
 
-function generateTickerText (quotes) {
-  return quotes
-    .map(quote => `$${quote.Symbol} : ${quote.ChangePercent}`)
-    .join('  ');
-}
-
-const MAX_FRAMES = 200;
-const FRAME_CHUNK_SIZE = 20;
-const FPS = 40;
-
 function generateFrames (mockData) {
   return new Promise(resolve => {
     function recurse (frames = []) {
@@ -94,6 +84,7 @@ function generateFrames (mockData) {
 function createTrayFrameAnimator (tray, fps) {
   var interval;
   var position = 0;
+
   return function animateTrayFrames (frames) {
     clearInterval(interval);
     interval = setInterval(() => {
@@ -105,34 +96,4 @@ function createTrayFrameAnimator (tray, fps) {
       }
     }, fps);
   }
-}
-
-function rotateTrayIcon (tray, mockData) {
-  var position = 0;
-  var interval;
-
-  generateFrames(mockData).then(frames => {
-    interval = setInterval(() => {
-      position += 1;
-      tray.setImage(frames[position]);
-
-      if (position >= MAX_FRAMES - 1) {
-        position = 0;
-      }
-    }, FPS);
-  });
-
-
-  return () => {
-    console.log('clearing interval', interval);
-    clearInterval(interval);
-  }
-}
-
-function buildStockMenu (stocks) {
-  var menuItems = stocks.map(stock => ({
-    label: stock.Symbol + ' ' + stock.ChangePercent.toFixed(2),
-    type: 'normal'
-  }));
-  return Menu.buildFromTemplate(menuItems);
 }
