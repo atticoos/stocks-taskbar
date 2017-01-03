8fe93ab4fe7724d498b52b4acd9d78db3fcdf9c0
Atticus White <contact@atticuswhite.com>
2017-01-02 22:08:23 -0500

    settings screen

---
+++ src/window/containers/settings.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+
+class Settings extends React.Component {
+  render () {
+    return (
+      <div className="settings">
+
+      </div>
+    );
+  }
+}
+
+export default connect()(Settings);
