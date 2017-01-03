2ef00810a6622d5c48801c75358ade0979570f8f
Atticus White <contact@atticuswhite.com>
2017-01-02 22:54:02 -0500

    settings layout

--- src/window/containers/settings.js
+++ src/window/containers/settings.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import {hashHistory} from 'react-router';
-import {Button, FormControl} from 'react-bootstrap';
+import {Button, ControlLabel, Form, FormControl, FormGroup} from 'react-bootstrap';
 import * as SettingActions from '../actions/settings';
 require('../styles/settings.less');
 
@@ -10,12 +10,18 @@ class Settings extends React.Component {
   render () {
     return (
       <div className="settings">
+        <Form onSubmit={e => e.preventDefault()}>
+          <h3>Ticker Settings</h3>
+          <FormGroup>
+            <ControlLabel>Ticker Width</ControlLabel>
+            <FormControl
+              type="number"
+              value={this.props.tickerWidth}
+              onChange={event => this.props.settingActions.setTickerWidth(event.target.value)}
+            />
+          </FormGroup>
+        </Form>
         <Button onClick={() => hashHistory.goBack()}>Back</Button>
-        <FormControl
-          type="number"
-          value={this.props.tickerWidth}
-          onChange={event => this.props.settingActions.setTickerWidth(event.target.value)}
-        />
       </div>
     );
   }
